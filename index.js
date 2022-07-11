3bcd707f5603169766ccbbdcd3ec3ad48f67e067
Lidia Kovac <lidia.kovac1998@gmail.com>
2022-07-11 20:11:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const demoEvent = function(clickEvent) {
+    console.log(clickEvent.target)
+}
+
+const demoKeyboard = function(keyboardEvent) {
+    // console.log(keyboardEvent.key)
+    console.log(keyboardEvent.target.value)
+    
+}
